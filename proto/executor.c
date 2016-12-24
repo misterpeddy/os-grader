f668b4f5396dcf717039f98ac066998468bd2a3c
Pedram Pejman <pedrampejman2010@gmail.com>
2016-12-23 23:08:58 -0500

    Added IPC to allow executor to send status reports to parent regarding compilation result

--- proto/executor.c
+++ proto/executor.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define DEBUG 1 
 
@@ -15,22 +17,30 @@
 #define TTY "/dev/tty"
 #define STAR "*****"
 
+const char COMP_AOK[] = "COMP_AOK\0";
+const char COMP_ERR[] = "COMP_ERR\0";
+const char RUN_AOK[] = "RUN_AOK\0";
+const char RUN_ERR[] = "RUN_ERR\0";
+
+char log_file[MAX_FILENAME_LEN];
+char err_file[MAX_FILENAME_LEN];
+
 void init_sandbox(char *user) 
 {	
+	if (DEBUG) printf("%sExecutor Started%s\n", STAR, STAR);
+	
 	char command[MAX_COMMAND_LEN];
 	memset(&command, 0, MAX_COMMAND_LEN);
 	sprintf(&command, "rm -rf %s && mkdir %s", user, user);
 	system(command);
 
 	// Log stdout
-	char logfile[MAX_FILENAME_LEN];
-	sprintf(&logfile, "%s/%s_%s", user, user, LOGFILE_SUFFIX);
-	freopen(logfile, "w", stdout);
+	sprintf(&log_file, "%s/%s_%s", user, user, LOGFILE_SUFFIX);
+	freopen(log_file, "w", stdout);
 
 	// Log stderr
-	memset(&logfile, 0, MAX_FILENAME_LEN);
-	sprintf(&logfile, "%s/%s_%s", user, user, ERRORFILE_SUFFIX);
-	freopen(logfile, "w", stderr);
+	sprintf(&err_file, "%s/%s_%s", user, user, ERRORFILE_SUFFIX);
+	freopen(err_file, "w", stderr);
 
 	if (DEBUG) 
 	{
@@ -39,17 +49,28 @@ void init_sandbox(char *user)
 	}
 }
 
-void compile_source(char *filename, char *user) 
+/* 
+** Compiles C file with name filename for user
+** Returns 0 if compiled with no errors otherwise positive number 
+*/
+int compile_source(char *filename, char *user) 
 {	
 	if (DEBUG) printf("\n%sCompiling %s for %s%s\n", STAR, filename, user, STAR);
 
+	// Compile source
 	char command[MAX_COMMAND_LEN];
 	memset(&command, 0, MAX_COMMAND_LEN);
-	sprintf(&command, "gcc %s -o %s/%s", filename, user, BIN);
+	sprintf(&command, "gcc -w %s -o %s/%s", filename, user, BIN);
 	if (DEBUG) printf("$(%s)\n", command);
 	system(command);
-
+	
+	// Read error log file stats
+	struct stat log_stat;
+	stat(err_file, &log_stat);
+	
 	if (DEBUG) printf("%sFinished compiling %s for %s%s\n\n", STAR, filename, user, STAR);
+
+	return log_stat.st_size;
 }
 
 void run_program(char *user) 
@@ -83,13 +104,14 @@ void clean_up()
 	fflush(stderr);
 	freopen(TTY, "a", stderr);
 	freopen(TTY, "a", stdout);
+	if (DEBUG) printf("%sExecutor Finished%s\n", STAR, STAR);
 }
 
 int main(int argc, char **argv) 
 {
 
 	// Check arguments
-	if (argc != 3) {
+	if (argc < 3) {
 		printf("Usage: %s <filename> <username>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
@@ -97,20 +119,30 @@ int main(int argc, char **argv)
 	// Capture arguments
 	char *filename = argv[1];
 	char *user = argv[2];
-
-	if (DEBUG) printf("****Executor Started****\n");
+	int pipe_fd = atoi(argv[3]);
 
 	// Create sandbox
 	init_sandbox(user);
 
-	// Compile submitted source code
-	compile_source(filename, user);
+	// Compile submitted source code 
+	int comp_result = compile_source(filename, user);
+
+	// Write compilation result code to pipe, exit if errored
+	if (comp_result)
+	{
+		write(pipe_fd, COMP_ERR, strlen(COMP_AOK));
+		clean_up();
+		exit(1);
+	}
+	else
+	{
+		write(pipe_fd, COMP_AOK, strlen(COMP_AOK));
+	}
 
 	// Run executable
 	run_program(user);
 
-	// Restore stdout
+	// Do any cleanup necessary 
 	clean_up();
 
-	if (DEBUG) printf("****Executor Finished****\n");
 }
--- proto/proto.c
+++ proto/proto.c
@@ -7,18 +7,44 @@
 
 int main() 
 {
+	int fd[2];
 	char filename[16];
+	char fd_w[32];
+	char user[8];
+
+	// Create shared pipe, fd[0] for reading, fd[1] for writing
+	pipe(fd);
+	sprintf(&fd_w, "%d", fd[1]);
+	
+	// Capture filename from argument 
 	strcpy(&filename, "prog.c");
-	char *exec_args[] = {EXECUTOR, &filename, "pp5nv", NULL};
+	
+	// Capture username from argument
+	strcpy(&user, "pp5nv");
+
+	// Set arguments and environment variables
+	char *exec_args[] = {EXECUTOR, &filename, user, fd_w, NULL};
 	char *exec_envs[] = {NULL};
 
 	if (fork() == 0) 
 	{
+		// Close reading end since executor will not read from the pipe
+		close(fd[0]);
+
 		if (DEBUG) printf("Starting Execution\n");
 		execv(EXECUTOR, exec_args);
 		perror("||execve||");
 	} else 
 	{
+		// Close writing end since receiver will not write to the pipe
+		close(fd[1]);	
+
+		int nbytes;
+		char buf[32];
+		nbytes = read(fd[0], buf, sizeof(buf));
+		printf("Received [%s]\n", buf);		
+
 		wait(NULL);
+		if (DEBUG) printf("Finished Execution\n");
 	}
 }
