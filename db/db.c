9065eb135aaf83bb5753b007e6e12e9c131b4076
Pedram Pejman <pedrampejman2010@gmail.com>
2017-01-05 20:50:51 -0500

    Implemented insert_record, lookup_user, lookup_assignment and SELECT callback

--- db/db.c
+++ db/db.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sqlite3.h> 
 
 #define MAX_FILENAME_LEN 128
 
+#define MAX_SQL_RESPONSE_LEN 1024
+#define MAX_SQL_COMMAND_LEN 512
 #define DB_PATH "test_database.db"
-#define MAX_SQL_COMMAND_LEN 1024
-#define DB_TABLE_NAME "SUBMISSIONS"
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-	int i;
-	for(i=0; i<argc; i++){
-		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-	}
-	printf("\n");
+#define DB_TABLE_NAME "SUBMISSIONS"
+#define DB_COL_USER "USER"
+#define DB_COL_ASSNUM "ASS_NUM"
+#define DB_COL_STATUS "STATUS"
+
+#define SQL_COL_DELIM "*"
+#define SQL_LINE_DELIM "\n"
+#define NULL_STR "NULL"
+
+/*
+** To be used as the callback to sqlite3_exec() for SELECT statements.
+** Assumes the first argument is a string, which the response is written to, with following format:
+** [<USER><SQL_COL_DELIM><ASSIGNMENT NUMBER><SQL_COL_DELIM><STATUS CODE><SQL_LINE_DELIM>]*
+** Non-zero return value would cause a SQLITE_ABORT return value for the responsible sqlite3_exec() call. 
+*/
+int record_retrieval_callback(void *passed_arg, int argc, char **argv, char **column_names){
+
+	// Find the first empty spot in the response buffer 
+	char *response_buffer = (char *)passed_arg;
+	int k=0;
+	while(response_buffer[k++]);
+	response_buffer += k - 1;	
+
+	// Write the response in the specified format
+	sprintf(response_buffer
+			,"%s%s%s%s%s%s" 
+			, argv[0] ? argv[0] : NULL_STR, SQL_COL_DELIM
+			, argv[1] ? argv[1] : NULL_STR, SQL_COL_DELIM
+			, argv[2] ? argv[2] : NULL_STR, SQL_LINE_DELIM);
+
 	return 0;
 }
 
@@ -44,15 +69,16 @@ int create_table(sqlite3 *db) {
 
 	// Create SQL statement 
 	char sql_command[MAX_SQL_COMMAND_LEN];
+	memset(sql_command, 0, MAX_SQL_COMMAND_LEN);
 	sprintf(sql_command, 
-      "CREATE TABLE %s("  \
-      "USER     CHAR(%d) PRIMARY KEY NOT NULL," \
-      "ASS_NUM  CHAR(32)             NOT NULL," \
-      "STATUS   INT                  NOT NULL);"
-      , DB_TABLE_NAME, MAX_FILENAME_LEN);
+      "CREATE TABLE %s("							/* DB_TABLE_NAME */ 
+      "%s     CHAR(%d)	NOT NULL," 		/* DB_COL_USER */
+      "%s  		CHAR(32)  NOT NULL,"  	/* DB_COL_ASSNUM */
+      "%s   	INT       NOT NULL);" 	/* DB_COL_STATUS */
+      , DB_TABLE_NAME, DB_COL_USER, MAX_FILENAME_LEN, DB_COL_ASSNUM, DB_COL_STATUS);
 
 	// Execute SQL statement
-	int return_code = sqlite3_exec(db, sql_command, callback, 0, &error_message);
+	int return_code = sqlite3_exec(db, sql_command, record_retrieval_callback, 0, &error_message);
 
   // Check return code
 	if(return_code != SQLITE_OK){
@@ -63,6 +89,98 @@ int create_table(sqlite3 *db) {
 
   printf("Table %s created successfully\n", DB_TABLE_NAME);
   return 0;
+}
+
+/*
+** Assuming a valid db connection, insert_record inserts a row
+** in DB_TABLE_NAME with the provided values. All arguments must be
+** non-NULL. Returns 1 on error and 0 otherwise.
+*/
+int insert_record(sqlite3 *db, char *user, char *ass_num, int status) {
+	// Declare error buffer and return code
+	char *error_message;
+	int return_code;
+
+	// Create SQL statement 
+	char sql_command[MAX_SQL_COMMAND_LEN];
+	memset(sql_command, 0, MAX_SQL_COMMAND_LEN);
+	sprintf(sql_command, 		
+			"INSERT INTO %s (%s, %s, %s) "
+			"VALUES ('%s', '%s', %d); "
+			, DB_TABLE_NAME 
+			, DB_COL_USER, DB_COL_ASSNUM, DB_COL_STATUS
+			, user, ass_num, status);
+
+	// Execute SQL statement
+	return_code = sqlite3_exec(db, sql_command, record_retrieval_callback, 0, &error_message);
+	if(return_code != SQLITE_OK) {
+		printf("SQL error: %s\n", error_message);
+		sqlite3_free(error_message);
+		return 1;
+	}
+
+	printf("Record inserted successfully\n");
+	return 0;
+}
+
+/*
+** Assuming a valid db connection, lookup_user looks up all entries for a user
+** in the database and writes the response to response_buffer with following format:
+** [<USER><SQL_COL_DELIM><ASSIGNMENT NUMBER><SQL_COL_DELIM><STATUS CODE><SQL_LINE_DELIM>]*
+** Returns 1 if any errors occur, otherwise 0.
+*/
+int lookup_user(sqlite3 *db, char *user, char *response_buffer) {
+	// Declare error buffer and return code
+	char *error_message;
+	int return_code;
+
+	// Create SQL statement
+	char sql_command[MAX_SQL_COMMAND_LEN];
+	memset(sql_command, 0, MAX_SQL_COMMAND_LEN);
+	sprintf(sql_command,
+			"SELECT * FROM %s WHERE %s='%s'"
+			, DB_TABLE_NAME, DB_COL_USER, user);
+	
+	// Execute SQL statement
+	return_code = sqlite3_exec(db, sql_command, record_retrieval_callback, response_buffer, &error_message);
+	if (return_code != SQLITE_OK) {
+		printf("SQL error: %s\n", error_message);
+		sqlite3_free(error_message);
+		return 1;
+	}
+
+	printf("Succesfully retrieved records for user %s\n", user);
+	return 0;
+}
+
+/*
+** Assuming a valid db connection, lookup_assignment looks up all entries for an assignment
+** in the database and writes the response to response_buffer with following format:
+** [<USER><SQL_COL_DELIM><ASSIGNMENT NUMBER><SQL_COL_DELIM><STATUS CODE><SQL_LINE_DELIM>]*
+** Returns 1 if any errors occur, otherwise 0.
+*/
+int lookup_assignment(sqlite3 *db, char *ass_num, char *response_buffer) {
+	// Declare error buffer and return code
+	char *error_message;
+	int return_code;
+
+	// Create SQL statement
+	char sql_command[MAX_SQL_COMMAND_LEN];
+	memset(sql_command, 0, MAX_SQL_COMMAND_LEN);
+	sprintf(sql_command,
+			"SELECT * FROM %s WHERE %s='%s'"
+			, DB_TABLE_NAME, DB_COL_ASSNUM, ass_num);
+	
+	// Execute SQL statement
+	return_code = sqlite3_exec(db, sql_command, record_retrieval_callback, response_buffer, &error_message);
+	if (return_code != SQLITE_OK) {
+		printf("SQL error: %s\n", error_message);
+		sqlite3_free(error_message);
+		return 1;
+	}
+
+	printf("Succesfully retrieved records for assignment %s\n", ass_num);
+	return 0;
 }
 
 /*
@@ -78,7 +196,7 @@ int close_db(sqlite3 *db) {
 
   return 0;
 }
-
+ 
 int main(int argc, char* argv[])
 {
 	// Declare handle to the database file
@@ -90,10 +208,21 @@ int main(int argc, char* argv[])
 
   // Create a submissions table 
   if (create_table(db)) {
-    close_db(db);
-    exit(EXIT_FAILURE);
+    //close_db(db);
+    //exit(EXIT_FAILURE);
   }
+	
+	// Insert a recrod into the table
+	insert_record(db, "kmv5tf", "1", 0);
+	insert_record(db, "pp5nv", "0", 1);
+	insert_record(db, "pp5nv", "0", 2);
 
+	// Look up the records for pp5nv
+	char response[MAX_SQL_RESPONSE_LEN];
+	memset(response, 0, MAX_SQL_RESPONSE_LEN);
+	lookup_user(db, "pp5nv", (char *)&response);
+	printf("Response: \n%s", response);
+	
 	close_db(db);
 
 	return 0;
