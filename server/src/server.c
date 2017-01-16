083f6eba0030f47fccf30752307b0d15b4e734c9
Pedram Pejman <pedrampejman2010@gmail.com>
2017-01-16 16:37:55 +0900

    Added std=c99 flag to all compilation rules

--- server/src/server.c
+++ server/src/server.c
@@ -1,10 +1,12 @@
 #include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "server.h"
 #include "macros.h"
